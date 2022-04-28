27b595a36585638b67d874ce216cb423ca11c909
Lev <lev.kokotov@gmail.com>
2022-04-28 07:18:33 -0700

    Maybe fix static files

--- pgml-admin/pgml_admin/settings.py
+++ pgml-admin/pgml_admin/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
