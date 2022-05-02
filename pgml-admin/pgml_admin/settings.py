f89c26a2c5d5b62f2bffb898ae1ac3bbb5f33f51
Montana Low <montanalow@gmail.com>
2022-05-02 09:38:21 -0700

    register mjs mimetype

--- pgml-admin/pgml_admin/settings.py
+++ pgml-admin/pgml_admin/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 import dotenv
 
 dotenv.load_dotenv()
+
+mimetypes.add_type('application/javascript', '.mjs')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
