50ba6c237227b5b0bdda9894fba8e8001a0738b5
Lev Kokotov <lev.kokotov@gmail.com>
2022-08-03 17:00:57 -0700

    Now its real auth

--- pgml-dashboard/app/views/root.py
+++ pgml-dashboard/app/views/root.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .. import models
+from app import models
 
 
 def index(request):
     projects = models.Project.objects.all()
     return render(request, "projects/index.html", {"title": "Projects", "projects": projects})
+
+
+def set_auth_cookie(request):
+    """Persist the user auth token in the cookie."""
+    token = request.GET.get("dashboard_auth")
+
+    if not token:
+        return HttpResponse(status=400)
+
+    response = HttpResponseRedirect(reverse("index"))
+    response.set_cookie("dashboard_auth", token, secure=True, httponly=True)
+
+    return response
