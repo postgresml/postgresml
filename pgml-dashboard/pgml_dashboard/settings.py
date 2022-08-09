1e25013ac5cc49e3110ef2564d8b2ba6810fc389
Lev Kokotov <levkk@users.noreply.github.com>
2022-08-09 15:20:11 -0700

    Allow cross-domain csrf (#195)

--- pgml-dashboard/pgml_dashboard/settings.py
+++ pgml-dashboard/pgml_dashboard/settings.py
@@ -40,6 +40,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
 ALLOWED_HOSTS = [x.strip() for x in os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost, 127.0.0.1").split(",")]
+CSRF_TRUSTED_ORIGINS = [x.strip() for x in os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost, http://127.0.0.1").split(",")]
 
 CONN_MAX_AGE = 60
 
