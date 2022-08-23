d3102b8dbb75f036bc7b20b44a150c09bbffd05f
Montana Low <montanalow@gmail.com>
2022-08-22 21:17:35 -0700

    relative urls with consistent hierarchy

--- pgml-dashboard/app/urls.py
+++ pgml-dashboard/app/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from rest_framework import routers
 
 from app.views import root, projects, models, snapshots, deployments, console, notebooks
+from notebooks.views import (
+    notebook,
+    add_notebook_cell,
+    remove_notebook_cell,
+    notebook_cell,
+    edit_notebook_cell,
+    undo_remove_notebook_cell,
+    create_notebook,
+    rename_notebook,
+    reset_notebook,
+    play_notebook_cell,
+)
 
 router = routers.DefaultRouter()
 router.register("projects", projects.ProjectViewSet)
@@ -16,7 +28,6 @@ html_router.register("snapshots/analysis", snapshots.SnapshotAnalysisView, basen
 
 urlpatterns = [
     path("", root.index, name="index"),
-    path("nb/", notebooks.index, name="notebooks"),
     path("deployments/", deployments.index, name="deployments"),
     path("deployments/<int:id>", deployments.deployment, name="deployment"),
     path("models/", models.ModelListView.as_view(), name="models"),
@@ -29,4 +40,20 @@ urlpatterns = [
     path("console/", console.ConsoleView.as_view(), name="console"),
     path("console/run/", console.run_sql, name="console/run-sql"),
     path("set-auth-cookie/", root.set_auth_cookie, name="set-auth-cookie"),
+
+    path("notebooks/", notebooks.index, name="notebooks"),
+    path("notebooks/<int:pk>/", notebook, name="notebooks/notebook"),
+    path("notebooks/create/", create_notebook, name="notebooks/create"),
+    path("notebooks/<int:pk>/rename/", rename_notebook, name="notebooks/rename"),
+    path("notebooks/<int:pk>/cell/add/", add_notebook_cell, name="notebooks/cell/add"),
+    path("notebooks/<int:notebook_pk>/cell/<int:cell_pk>/", notebook_cell, name="notebooks/cell/get"),
+    path("notebooks/<int:notebook_pk>/cell/<int:cell_pk>/edit/", edit_notebook_cell, name="notebooks/cell/edit"),
+    path("notebooks/<int:notebook_pk>/cell/<int:cell_pk>/remove/", remove_notebook_cell, name="notebooks/cell/remove"),
+    path(
+        "notebooks/<int:notebook_pk>/cell/<int:cell_pk>/remove/undo/",
+        undo_remove_notebook_cell,
+        name="notebooks/cell/remove/undo",
+    ),
+    path("notebooks/<int:pk>/reset/", reset_notebook, name="notebooks/reset"),
+    path("notebooks/<int:notebook_pk>/cell/<int:cell_pk>/play/", play_notebook_cell, name="notebooks/cell/play"),
 ]
--- pgml-dashboard/notebooks/urls.py
+++ pgml-dashboard/notebooks/urls.py
@@ -1,31 +1,5 @@
 from django.urls import path
 
-from notebooks.views import (
-    notebook,
-    add_notebook_cell,
-    remove_notebook_cell,
-    notebook_cell,
-    edit_notebook_cell,
-    undo_remove_notebook_cell,
-    create_notebook,
-    rename_notebook,
-    reset_notebook,
-    play_notebook_cell,
-)
 
 urlpatterns = [
-    path("notebook/<int:pk>/", notebook, name="notebooks/notebook"),
-    path("create/", create_notebook, name="notebooks/create"),
-    path("notebook/<int:pk>/rename/", rename_notebook, name="notebooks/rename"),
-    path("notebook/<int:pk>/cell/add/", add_notebook_cell, name="notebooks/cell/add"),
-    path("notebook/<int:notebook_pk>/cell/<int:cell_pk>/", notebook_cell, name="notebooks/cell/get"),
-    path("notebook/<int:notebook_pk>/cell/<int:cell_pk>/edit/", edit_notebook_cell, name="notebooks/cell/edit"),
-    path("notebook/<int:notebook_pk>/cell/<int:cell_pk>/remove/", remove_notebook_cell, name="notebooks/cell/remove"),
-    path(
-        "notebook/<int:notebook_pk>/cell/<int:cell_pk>/remove/undo/",
-        undo_remove_notebook_cell,
-        name="notebooks/cell/remove/undo",
-    ),
-    path("notebook/<int:pk>/reset/", reset_notebook, name="notebooks/reset"),
-    path("notebook/<int:notebook_pk>/cell/<int:cell_pk>/play/", play_notebook_cell, name="notebooks/cell/play"),
 ]
