f9131d538e69bfed32234a29eb91fd9f44c0aaa3
Lev Kokotov <lev.kokotov@gmail.com>
2022-09-22 11:01:01 -0700

    Classification working too

--- pgml-extension/pgml_rust/src/engines/lightgbm.rs
+++ pgml-extension/pgml_rust/src/engines/lightgbm.rs
@@ -27,7 +27,7 @@ pub fn lightgbm_train(task: Task, dataset: &Dataset, hyperparams: &Hyperparams) 
                 );
                 hyperparams.insert(
                     "num_class".to_string(),
-                    serde_json::Value::from(dataset.distinct_labels()),
+                    serde_json::Value::from(distinct_labels),
                 ); // [0, num_class)
             } else {
                 hyperparams.insert("objective".to_string(), serde_json::Value::from("binary"));
@@ -66,6 +66,9 @@ pub fn lightgbm_load(data: &Vec<u8>) -> LightgbmBox {
     std::fs::write(&path, &data).unwrap();
 
     let bst = lightgbm::Booster::from_file(&path).unwrap();
+
+    std::fs::remove_file(&path).unwrap();
+
     LightgbmBox::new(bst)
 }
 
@@ -74,12 +77,50 @@ pub fn lightgbm_test(estimator: &LightgbmBox, dataset: &Dataset) -> Vec<f32> {
     let x_test = dataset.x_test();
     let num_features = dataset.num_features;
 
-    let y_hat = estimator.predict(&x_test, num_features as i32).unwrap();
-    let y_hat: Vec<f32> = y_hat.into_iter().map(|y| y as f32).collect();
+    assert_eq!(dataset.num_test_rows, x_test.len() / dataset.num_features);
+
+    let probabilities = estimator.predict(&x_test, num_features as i32).unwrap();
+    let num_class = estimator.num_class().unwrap();
+
+    // Given the proabilities of each class, find the most likely
+    // and return that as the result.
+    let y_hat: Vec<f32> = probabilities
+        .as_slice()
+        .chunks(num_class as usize)
+        .map(|chunk| {
+            let mut max = 0.0;
+            let mut answer = 0;
+            let mut it = 0;
+            for class_prob in chunk {
+                if *class_prob > max {
+                    max = *class_prob;
+                    answer = it;
+                }
+                it += 1;
+            }
+
+            answer as f32
+        })
+        .collect();
+
     y_hat
 }
 
 /// Predict a novel datapoint using the LightGBM estimator.
 pub fn lightgbm_predict(estimator: &LightgbmBox, x: &[f32]) -> f32 {
-    estimator.predict(&x, x.len() as i32).unwrap()[0] as f32
+    let probabilities = estimator.predict(&x, x.len() as i32).unwrap();
+
+    let mut max = 0.0;
+    let mut it = 0;
+    let mut answer = 0;
+
+    for class_prob in &probabilities {
+        if *class_prob > max {
+            max = *class_prob;
+            answer = it;
+        }
+        it += 1;
+    }
+
+    answer as f32
 }
