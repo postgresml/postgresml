120a000577ee839f47220e59bcab7f6015220779
Lev Kokotov <levkk@users.noreply.github.com>
2022-10-06 14:19:22 -0700

    SVM-ish in Linfa (#369)

--- pgml-extension/pgml_rust/src/bindings/linfa.rs
+++ pgml-extension/pgml_rust/src/bindings/linfa.rs
@@ -169,3 +169,62 @@ impl Bindings for LogisticRegression {
         rmp_serde::to_vec(self).unwrap()
     }
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Svm {
+    estimator: linfa_svm::Svm<f32, f32>,
+    num_features: usize,
+}
+
+impl Svm {
+    pub fn fit(dataset: &Dataset, _hyperparams: &Hyperparams) -> Box<dyn Bindings> {
+        let records = ArrayView2::from_shape(
+            (dataset.num_train_rows, dataset.num_features),
+            &dataset.x_train,
+        )
+        .unwrap();
+
+        let targets = ArrayView1::from_shape(dataset.num_train_rows, &dataset.y_train).unwrap();
+
+        let linfa_dataset = linfa::DatasetBase::from((records, targets));
+        let estimator = linfa_svm::Svm::params();
+        let estimator = estimator.fit(&linfa_dataset).unwrap();
+
+        Box::new(Svm {
+            estimator,
+            num_features: dataset.num_features,
+        })
+    }
+}
+
+impl Bindings for Svm {
+    /// Predict a novel datapoint.
+    fn predict(&self, features: &[f32]) -> f32 {
+        self.predict_batch(features)[0]
+    }
+
+    /// Predict a novel datapoint.
+    fn predict_batch(&self, features: &[f32]) -> Vec<f32> {
+        let records = ArrayView2::from_shape(
+            (features.len() / self.num_features, self.num_features),
+            features,
+        )
+        .unwrap();
+
+        self.estimator.predict(records).targets.into_raw_vec()
+    }
+
+    /// Deserialize self from bytes, with additional context
+    fn from_bytes(bytes: &[u8]) -> Box<dyn Bindings>
+    where
+        Self: Sized,
+    {
+        let estimator: Svm = rmp_serde::from_read(bytes).unwrap();
+        Box::new(estimator)
+    }
+
+    /// Serialize self to bytes
+    fn to_bytes(&self) -> Vec<u8> {
+        rmp_serde::to_vec(self).unwrap()
+    }
+}
