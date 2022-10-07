26ad12746e500c479a3aecdd464fb2eb14d9e8f3
Lev Kokotov <lev.kokotov@gmail.com>
2022-10-07 10:39:13 -0700

    disable slwo lightgbm for now

--- pgml-extension/examples/multi_classification.sql
+++ pgml-extension/examples/multi_classification.sql
@@ -48,7 +48,7 @@ SELECT * FROM pgml.train('Iris Flower Types', algorithm => 'random_forest', hype
 -- gradient boosting
 SELECT * FROM pgml.train('Iris Flower Types', algorithm => 'xgboost', hyperparams => '{"n_estimators": 10}');
 SELECT * FROM pgml.train('Iris Flower Types', algorithm => 'xgboost_random_forest', hyperparams => '{"n_estimators": 10}');
-SELECT * FROM pgml.train('Iris Flower Types', algorithm => 'lightgbm', hyperparams => '{"n_estimators": 1}');
+-- SELECT * FROM pgml.train('Iris Flower Types', algorithm => 'lightgbm', hyperparams => '{"n_estimators": 1}');
 -- Histogram Gradient Boosting is too expensive for normal tests on even a toy dataset
 -- SELECT * FROM pgml.train('Iris Flower Types', algorithim => 'hist_gradient_boosting', hyperparams => '{"max_iter": 2}');
 
