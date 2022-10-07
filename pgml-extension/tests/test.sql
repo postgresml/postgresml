ee27aedeec2fd0a68f19a067e4ad1fe66a9852e7
Lev Kokotov <lev.kokotov@gmail.com>
2022-10-06 22:20:03 -0700

    hmm

--- pgml-extension/tests/test.sql
+++ pgml-extension/tests/test.sql
@@ -5,22 +5,11 @@
 
 \timing
 
-SELECT pgml.version();
--- SELECT pgml.torch_cuda_available();
-
 SELECT pgml.load_dataset('diabetes');
 SELECT pgml.load_dataset('digits');
 SELECT pgml.load_dataset('iris');
-SELECT pgml.load_dataset('linnerud');
-SELECT pgml.load_dataset('wine');
 SELECT pgml.load_dataset('breast_cancer');
 
-SELECT pgml.load_dataset('california_housing');
-
--- \i examples/joint_regression.sql
-\i examples/vectors.sql
-\i examples/regression.sql
-\i examples/binary_classification.sql
-\i examples/multi_classification.sql
-\i examples/image_classification.sql
--- \i examples/transformers.sql
+\i pgml_rust/tests/regression.sql
+\i pgml_rust/tests/binary_classification.sql
+\i pgml_rust/tests/image_classification.sql
