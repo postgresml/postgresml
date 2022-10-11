8a5219d945234b227191e16db51a0c2853d9b38a
Montana Low <montanalow@gmail.com>
2022-10-10 18:46:58 -0700

    complete v2 end-to-end suite

--- pgml-extension/tests/test.sql
+++ pgml-extension/tests/test.sql
@@ -5,11 +5,16 @@
 
 \timing
 
+SELECT pgml.load_dataset('breast_cancer');
 SELECT pgml.load_dataset('diabetes');
 SELECT pgml.load_dataset('digits');
 SELECT pgml.load_dataset('iris');
-SELECT pgml.load_dataset('breast_cancer');
+SELECT pgml.load_dataset('linnerud');
+SELECT pgml.load_dataset('wine');
 
-\i tests/regression.sql
 \i tests/binary_classification.sql
 \i tests/image_classification.sql
+\i tests/joint_regression.sql
+\i tests/multi_classification.sql
+\i tests/regression.sql
+\i tests/vectors.sql
