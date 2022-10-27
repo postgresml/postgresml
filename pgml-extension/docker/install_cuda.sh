721576eb7c6bff5e3be950aa47a92c4e02f42332
Lev Kokotov <levkk@users.noreply.github.com>
2022-10-27 15:15:46 -0700

    Multi-arch cuda (#464)

---
+++ pgml-extension/docker/install_cuda.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Install CUDA.
+#
+
+if [[ $(uname -a) == *"aarch64"* ]]; then
+    ARCH="arm64"
+else
+    ARCH="amd64"
+fi
+
+# ARM
+if [[ "$(uname)" == "aarch64" ]]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/sbsa/cuda-keyring_1.0-1_all.deb
+fi
+
+# Intel
+if [[ "$(uname)" != "aarch64" ]]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+fi
+
+dpkg -i cuda-keyring_1.0-1_all.deb
+apt-get update
+apt-get -y install cuda
