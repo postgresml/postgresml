ef0451fac85b2d3324ca5b27fb420b11ea8699db
Lev Kokotov <lev.kokotov@gmail.com>
2022-10-27 16:31:30 -0700

    come on..cuda!

--- pgml-extension/docker/install_cuda.sh
+++ pgml-extension/docker/install_cuda.sh
@@ -11,12 +11,12 @@ fi
 
 # ARM
 if [[ ${ARCH} == "arm64" ]]; then
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/sbsa/cuda-keyring_1.0-1_all.deb
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION//.}/sbsa/cuda-keyring_1.0-1_all.deb
 fi
 
 # Intel
 if [[ ${ARCH} == "amd64" ]]; then
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION//.}/x86_64/cuda-keyring_1.0-1_all.deb
 fi
 
 dpkg -i cuda-keyring_1.0-1_all.deb
