1e39fef285005ef6a2207ce9bd22975a412556b1
santiatpml <124207123+santiatpml@users.noreply.github.com>
2023-02-27 11:19:03 -0800

    Dashboard backend tests (#557)

--- pgml-dashboard/src/main.rs
+++ pgml-dashboard/src/main.rs
@@ -32,26 +32,40 @@ async fn main() {
 
 #[cfg(test)]
 mod test {
-    use rocket::local::asynchronous::Client;
     use pgml_dashboard::Clusters;
     use pgml_dashboard::{index, migrate, paths};
     use rocket::fs::FileServer;
+    use rocket::local::asynchronous::Client;
     use rocket::{Build, Rocket};
-
+    use scraper::{Html, Selector};
+    use std::vec::Vec;
 
     async fn rocket() -> Rocket<Build> {
         let clusters = Clusters::new();
         clusters
             .add(-1, &pgml_dashboard::guards::default_database_url())
             .unwrap();
-    
+
         migrate(&clusters.get(-1).unwrap()).await.unwrap();
-    
+
         rocket::build()
             .manage(clusters)
             .mount("/", rocket::routes![index,])
             .mount("/dashboard/static", FileServer::from("static"))
             .mount("/dashboard", paths())
+    }
+
+    fn get_href_links(body: &str, pattern: &str) -> Vec<String> {
+        let document = Html::parse_document(body);
+        let selector = Selector::parse("a").unwrap();
+        let mut output = Vec::<String>::new();
+        for element in document.select(&selector) {
+            let href = element.value().attr("href").unwrap();
+            if href.contains(pattern) && href != pattern {
+                output.push(String::from(href));
+            }
+        }
+        output
     }
 
     #[rocket::async_test]
@@ -95,5 +109,79 @@ mod test {
         let response = client.get("/dashboard/snapshots/").dispatch().await;
         assert_eq!(response.status().code, 200);
     }
-}
 
+    #[rocket::async_test]
+    async fn test_snapshot_entries() {
+        let snapshots_endpoint = "/dashboard/snapshots/";
+        let client = Client::tracked(rocket().await).await.unwrap();
+        let response = client.get(snapshots_endpoint).dispatch().await;
+
+        let body = response.into_string().await.unwrap();
+        let snapshot_links = get_href_links(body.as_str(), snapshots_endpoint);
+
+        for link in snapshot_links {
+            let response = client.get(link.as_str()).dispatch().await;
+            assert_eq!(response.status().code, 200);
+        }
+    }
+
+    #[rocket::async_test]
+    async fn test_notebook_entries() {
+        let notebooks_endpoint = "/dashboard/notebooks/";
+        let client = Client::tracked(rocket().await).await.unwrap();
+        let response = client.get(notebooks_endpoint).dispatch().await;
+
+        let body = response.into_string().await.unwrap();
+        let notebook_links = get_href_links(body.as_str(), notebooks_endpoint);
+
+        for link in notebook_links {
+            let response = client.get(link.as_str()).dispatch().await;
+            assert_eq!(response.status().code, 200);
+        }
+    }
+
+    #[rocket::async_test]
+    async fn test_project_entries() {
+        let projects_endpoint = "/dashboard/projects/";
+        let client = Client::tracked(rocket().await).await.unwrap();
+        let response = client.get(projects_endpoint).dispatch().await;
+
+        let body = response.into_string().await.unwrap();
+        let project_links = get_href_links(body.as_str(), projects_endpoint);
+
+        for link in project_links {
+            let response = client.get(link.as_str()).dispatch().await;
+            assert_eq!(response.status().code, 200);
+        }
+    }
+
+    #[rocket::async_test]
+    async fn test_model_entries() {
+        let models_endpoint = "/dashboard/models/";
+        let client = Client::tracked(rocket().await).await.unwrap();
+        let response = client.get(models_endpoint).dispatch().await;
+
+        let body = response.into_string().await.unwrap();
+        let model_links = get_href_links(body.as_str(), models_endpoint);
+
+        for link in model_links {
+            let response = client.get(link.as_str()).dispatch().await;
+            assert_eq!(response.status().code, 200);
+        }
+    }
+
+    #[rocket::async_test]
+    async fn test_deployment_entries() {
+        let deployments_endpoint = "/dashboard/deployments/";
+        let client = Client::tracked(rocket().await).await.unwrap();
+        let response = client.get(deployments_endpoint).dispatch().await;
+
+        let body = response.into_string().await.unwrap();
+        let deployment_links = get_href_links(body.as_str(), deployments_endpoint);
+
+        for link in deployment_links {
+            let response = client.get(link.as_str()).dispatch().await;
+            assert_eq!(response.status().code, 200);
+        }
+    }
+}
