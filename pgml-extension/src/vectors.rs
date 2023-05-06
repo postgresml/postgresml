c5f7751bfe0ae70d2b2de2a3e0d3a553e0b480be
Montana Low <montana.low@gmail.com>
2023-05-06 15:24:21 -0700

    fmt

--- pgml-extension/src/vectors.rs
+++ pgml-extension/src/vectors.rs
@@ -1,5 +1,5 @@
-use pgrx::*;
 use pgrx::array::RawArray;
+use pgrx::*;
 
 #[pg_extern(immutable, parallel_safe, strict, name = "add")]
 fn add_scalar_s(vector: Array<f32>, addend: f32) -> Vec<f32> {
@@ -43,7 +43,8 @@ fn divide_scalar_d(vector: Array<f64>, dividend: f64) -> Vec<f64> {
 
 #[pg_extern(immutable, parallel_safe, strict, name = "add")]
 fn add_vector_s(vector: Array<f32>, addend: Array<f32>) -> Vec<f32> {
-    vector.iter_deny_null()
+    vector
+        .iter_deny_null()
         .zip(addend.iter_deny_null())
         .map(|(a, b)| a + b)
         .collect()
@@ -283,13 +284,7 @@ fn dot_product_s(vector: Array<f32>, other: Array<f32>) -> f32 {
     unsafe {
         let vector: &[f32] = RawArray::from_array(vector).unwrap().data().as_ref();
         let other: &[f32] = RawArray::from_array(other).unwrap().data().as_ref();
-        blas::sdot(
-            vector.len().try_into().unwrap(),
-            vector,
-            1,
-            other,
-            1,
-        )
+        blas::sdot(vector.len().try_into().unwrap(), vector, 1, other, 1)
     }
 }
 
@@ -298,13 +293,7 @@ fn dot_product_d(vector: Array<f64>, other: Array<f64>) -> f64 {
     unsafe {
         let vector: &[f64] = RawArray::from_array(vector).unwrap().data().as_ref();
         let other: &[f64] = RawArray::from_array(other).unwrap().data().as_ref();
-        blas::ddot(
-            vector.len().try_into().unwrap(),
-            vector,
-            1,
-            other,
-            1,
-        )
+        blas::ddot(vector.len().try_into().unwrap(), vector, 1, other, 1)
     }
 }
 
@@ -1051,49 +1040,65 @@ mod tests {
 
     #[pg_test]
     fn test_add_vector_s() {
-        let result = Spi::get_one::<Vec<f32>>("SELECT pgml.add(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])");
+        let result = Spi::get_one::<Vec<f32>>(
+            "SELECT pgml.add(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some([2.0, 4.0, 6.0].to_vec())));
     }
 
     #[pg_test]
     fn test_add_vector_d() {
-        let result = Spi::get_one::<Vec<f64>>("SELECT pgml.add(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])");
+        let result = Spi::get_one::<Vec<f64>>(
+            "SELECT pgml.add(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some([2.0, 4.0, 6.0].to_vec())));
     }
 
     #[pg_test]
     fn test_subtract_vector_s() {
-        let result = Spi::get_one::<Vec<f32>>("SELECT pgml.subtract(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])");
+        let result = Spi::get_one::<Vec<f32>>(
+            "SELECT pgml.subtract(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some([0.0, 0.0, 0.0].to_vec())));
     }
 
     #[pg_test]
     fn test_subtract_vector_d() {
-        let result = Spi::get_one::<Vec<f64>>("SELECT pgml.subtract(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])");
+        let result = Spi::get_one::<Vec<f64>>(
+            "SELECT pgml.subtract(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some([0.0, 0.0, 0.0].to_vec())));
     }
 
     #[pg_test]
     fn test_multiply_vector_s() {
-        let result = Spi::get_one::<Vec<f32>>("SELECT pgml.subtract(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])");
+        let result = Spi::get_one::<Vec<f32>>(
+            "SELECT pgml.subtract(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some([0.0, 0.0, 0.0].to_vec())));
     }
 
     #[pg_test]
     fn test_multiply_vector_d() {
-        let result = Spi::get_one::<Vec<f64>>("SELECT pgml.multiply(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])");
+        let result = Spi::get_one::<Vec<f64>>(
+            "SELECT pgml.multiply(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some([1.0, 4.0, 9.0].to_vec())));
     }
 
     #[pg_test]
     fn test_divide_vector_s() {
-        let result = Spi::get_one::<Vec<f32>>("SELECT pgml.divide(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])");
+        let result = Spi::get_one::<Vec<f32>>(
+            "SELECT pgml.divide(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some([1.0, 1.0, 1.0].to_vec())));
     }
 
     #[pg_test]
     fn test_divide_vector_d() {
-        let result = Spi::get_one::<Vec<f64>>("SELECT pgml.divide(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])");
+        let result = Spi::get_one::<Vec<f64>>(
+            "SELECT pgml.divide(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some([1.0, 1.0, 1.0].to_vec())));
     }
 
@@ -1160,19 +1165,32 @@ mod tests {
     #[pg_test]
     fn test_normalize_l1_d() {
         let result = Spi::get_one::<Vec<f64>>("SELECT pgml.normalize_l1(ARRAY[1,2,3]::float8[])");
-        assert_eq!(result, Ok(Some([0.16666666666666666, 0.3333333333333333, 0.5].to_vec())));
+        assert_eq!(
+            result,
+            Ok(Some(
+                [0.16666666666666666, 0.3333333333333333, 0.5].to_vec()
+            ))
+        );
     }
 
     #[pg_test]
     fn test_normalize_l2_s() {
         let result = Spi::get_one::<Vec<f32>>("SELECT pgml.normalize_l2(ARRAY[1,2,3]::float4[])");
-        assert_eq!(result, Ok(Some([0.26726124, 0.5345225, 0.8017837].to_vec())));
+        assert_eq!(
+            result,
+            Ok(Some([0.26726124, 0.5345225, 0.8017837].to_vec()))
+        );
     }
 
     #[pg_test]
     fn test_normalize_l2_d() {
         let result = Spi::get_one::<Vec<f64>>("SELECT pgml.normalize_l2(ARRAY[1,2,3]::float8[])");
-        assert_eq!(result, Ok(Some([0.2672612419124244, 0.5345224838248488, 0.8017837257372732].to_vec())));
+        assert_eq!(
+            result,
+            Ok(Some(
+                [0.2672612419124244, 0.5345224838248488, 0.8017837257372732].to_vec()
+            ))
+        );
     }
 
     #[pg_test]
@@ -1184,57 +1202,80 @@ mod tests {
     #[pg_test]
     fn test_normalize_max_d() {
         let result = Spi::get_one::<Vec<f64>>("SELECT pgml.normalize_max(ARRAY[1,2,3]::float8[])");
-        assert_eq!(result, Ok(Some([0.3333333333333333, 0.6666666666666666, 1.0].to_vec())));
+        assert_eq!(
+            result,
+            Ok(Some([0.3333333333333333, 0.6666666666666666, 1.0].to_vec()))
+        );
     }
 
     #[pg_test]
     fn test_distance_l1_s() {
-        let result = Spi::get_one::<f32>("SELECT pgml.distance_l1(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.distance_l1(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])",
+        );
         assert_eq!(result, Ok(Some(0.0)));
     }
 
     #[pg_test]
     fn test_distance_l1_d() {
-        let result = Spi::get_one::<f64>("SELECT pgml.distance_l1(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.distance_l1(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])",
+        );
         assert_eq!(result, Ok(Some(0.0)));
     }
 
     #[pg_test]
     fn test_distance_l2_s() {
-        let result = Spi::get_one::<f32>("SELECT pgml.distance_l2(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.distance_l2(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])",
+        );
         assert_eq!(result, Ok(Some(0.0)));
     }
 
     #[pg_test]
     fn test_distance_l2_d() {
-        let result = Spi::get_one::<f64>("SELECT pgml.distance_l2(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.distance_l2(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])",
+        );
         assert_eq!(result, Ok(Some(0.0)));
     }
 
     #[pg_test]
     fn test_dot_product_s() {
-        let result = Spi::get_one::<f32>("SELECT pgml.dot_product(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.dot_product(ARRAY[1,2,3]::float4[],ARRAY[1,2,3]::float4[])",
+        );
         assert_eq!(result, Ok(Some(14.0)));
 
-        let result = Spi::get_one::<f32>("SELECT pgml.dot_product(ARRAY[1,2,3]::float4[],ARRAY[2,3,4]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.dot_product(ARRAY[1,2,3]::float4[],ARRAY[2,3,4]::float4[])",
+        );
         assert_eq!(result, Ok(Some(20.0)));
     }
 
     #[pg_test]
     fn test_dot_product_d() {
-        let result = Spi::get_one::<f64>("SELECT pgml.dot_product(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.dot_product(ARRAY[1,2,3]::float8[],ARRAY[1,2,3]::float8[])",
+        );
         assert_eq!(result, Ok(Some(14.0)));
 
-        let result = Spi::get_one::<f64>("SELECT pgml.dot_product(ARRAY[1,2,3]::float8[],ARRAY[2,3,4]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.dot_product(ARRAY[1,2,3]::float8[],ARRAY[2,3,4]::float8[])",
+        );
         assert_eq!(result, Ok(Some(20.0)));
     }
 
     #[pg_test]
     fn test_cosine_similarity_s() {
-        let result = Spi::get_one::<f32>("SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float4[], ARRAY[1.0, 2.0, 3.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some(0.99999994)));
 
-        let result = Spi::get_one::<f32>("SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float4[], ARRAY[2.0, 3.0, 4.0]::float4[])");
+        let result = Spi::get_one::<f32>(
+            "SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float4[], ARRAY[2.0, 3.0, 4.0]::float4[])",
+        );
         assert_eq!(result, Ok(Some(0.9925833)));
 
         let result = Spi::get_one::<f32>("SELECT pgml.cosine_similarity(ARRAY[1,1,1,1,1,0,0]::float4[], ARRAY[0,0,1,1,0,1,1]::float4[])");
@@ -1243,10 +1284,14 @@ mod tests {
 
     #[pg_test]
     fn test_cosine_similarity_d() {
-        let result = Spi::get_one::<f64>("SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float8[], ARRAY[1.0, 2.0, 3.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some(1.0)));
 
-        let result = Spi::get_one::<f64>("SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float8[], ARRAY[2.0, 3.0, 4.0]::float8[])");
+        let result = Spi::get_one::<f64>(
+            "SELECT pgml.cosine_similarity(ARRAY[1,2,3]::float8[], ARRAY[2.0, 3.0, 4.0]::float8[])",
+        );
         assert_eq!(result, Ok(Some(0.9925833339709303)));
 
         let result = Spi::get_one::<f64>("SELECT pgml.cosine_similarity(ARRAY[1,1,1,1,1,0,0]::float8[], ARRAY[0,0,1,1,0,1,1]::float8[])");
