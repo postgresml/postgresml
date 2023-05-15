91797d794347ccfa25812264d69ed5f38ab59d4c
Montana Low <montana.low@gmail.com>
2023-05-15 13:56:23 -0700

    path

--- pgml-dashboard/src/utils/markdown.rs
+++ pgml-dashboard/src/utils/markdown.rs
@@ -954,6 +954,7 @@ impl SearchIndex {
             let path_field = schema.get_field("path").unwrap();
             let title_regex_field = schema.get_field("title_regex").unwrap();
 
+            info!("found path: {path}");
             let path = path
                 .to_str()
                 .unwrap()
