0f3bf87917f54bd7480b052e228892b983f56d65
Santi Adavani <santis@gmail.com>
2023-06-08 15:05:23 -0700

    Added pgml.transform to queries

--- pgml-sdks/python/pgml/pgml/queries.py
+++ pgml-sdks/python/pgml/pgml/queries.py
@@ -1,7 +1,8 @@
 from typing import Any
-from pypika.functions import Function
-from typing import Dict
-from pypika import JSON, Array
+from pypika.functions import Function, Cast
+from typing import Dict, List
+from pypika import JSON, Array, Field
+import json
 
 
 class Embed(Function):
@@ -20,3 +21,29 @@ class Embed(Function):
 class CosineDistance(Function):
     def __init__(self, lhs: Array, rhs: Array, alias: str = "cosine") -> None:
         super(CosineDistance, self).__init__("cosine_distance", lhs, rhs, alias=alias)
+
+
+class Transform(Function):
+    def __init__(
+        self,
+        task: str | Dict[str, Any],
+        inputs: List,
+        args: Dict[str, Any] = {},
+        alias: str = "transform",
+    ) -> None:
+        super(Transform, self).__init__(
+            "pgml.transform", task=task, inputs=inputs, args=args, alias=alias
+        )
+        self.task = task
+        self.inputs = inputs
+        self.args = args
+
+    def get_function_sql(self, **kwargs: Any) -> str:
+        if isinstance(self.task, str):
+            return "pgml.transform(task => '{}', inputs => ARRAY{}, args => '{}'::JSONB)".format(
+                self.task, self.inputs, self.args
+            )
+        elif isinstance(self.task, dict):
+            return "pgml.transform(task => '{}'::JSONB, inputs => ARRAY{}, args => '{}'::JSONB)".format(
+                json.dumps(self.task), self.inputs, self.args
+            )
