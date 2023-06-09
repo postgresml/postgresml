579385f8928e0b7c0432831d4df2da7fffdb1359
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2023-06-09 16:45:34 -0700

    Clean up file names

--- pgml-sdks/rust/pgml/create_python_module.sh
+++ pgml-sdks/rust/pgml/create_python_module.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/bash
 
 # Remove old dist directory and create new one
-mkdir ../../python/pgml-sdk/dist/
-rm -r ../../python/pgml-sdk/dist/*
+mkdir ../../python/pgml/dist/
+rm -r ../../python/pgml/dist/*
 
 # Build wheel
-maturin build -r -o ../../python/pgml-sdk/dist/
+maturin build -r -o ../../python/pgml/dist/
 
 # Copy pyproject.toml over
-rm ../../python/pgml-sdk/pyproject.toml
-cp pyproject.toml ../../python/pgml-sdk/
+rm ../../python/pgml/pyproject.toml
+cp pyproject.toml ../../python/pgml/
 
 echo "Wheel built and files moved over"
 echo "Uploading to pypi"
 
 # Do the upload
-maturin upload --repository testpypi ../../python/pgml-sdk/dist/*
+maturin upload --repository testpypi ../../python/pgml/dist/*
