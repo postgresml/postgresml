275905aac10c0986613b308deae1d2f92d17393c
Lev Kokotov <hi@levthe.dev>
2023-06-12 15:37:14 -0700

    remove debug

--- pgml-dashboard/src/api/docs.rs
+++ pgml-dashboard/src/api/docs.rs
@@ -131,8 +131,6 @@ async fn render<'a>(
 ) -> Result<ResponseOk, Status> {
     let url = path.clone();
 
-    println!("Path: {:?}", url);
-
     // Get the document content
     let path = Path::new(&config::content_dir())
         .join(folder)
