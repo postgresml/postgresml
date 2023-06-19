dc049b4244cc3c960d878d855b5adc7e274349b9
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2023-06-19 16:34:05 -0700

    Working manual build

--- pgml-sdks/rust/pgml/python/manual-build-deploy.sh
+++ pgml-sdks/rust/pgml/python/manual-build-deploy.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+echo "Make sure and set the environment variable MATURIN_PYPI_TOKEN to your PyPI token."
 
 cd ..
 maturin build --release --strip -i python3.8 -i python3.9 -i python3.10 -i python3.11 -o ../../python/pgml/dist
