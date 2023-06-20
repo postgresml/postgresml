cba5448d5d54950e04cde07329a7185b835e20eb
santiatpml <124207123+santiatpml@users.noreply.github.com>
2023-06-19 17:59:30 -0700

    manual build update (#764)

--- pgml-sdks/rust/pgml/python/manual-build-deploy.sh
+++ pgml-sdks/rust/pgml/python/manual-build-deploy.sh
@@ -2,7 +2,10 @@
 
 echo "Make sure and set the environment variable MATURIN_PYPI_TOKEN to your PyPI token."
 
+
 cd ..
+rm -r ../../python/pgml/dist/
+mkdir ../../python/pgml/dist/
 maturin build --release --strip -i python3.8 -i python3.9 -i python3.10 -i python3.11 -o ../../python/pgml/dist
 cd ../../python/pgml
-maturin upload --repository testpypi --skip-existing dist/*
+maturin upload --skip-existing dist/*
