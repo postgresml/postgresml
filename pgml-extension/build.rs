c273ee77d226fa8d7a78cd9d46870231cd191884
Lev Kokotov <lev.kokotov@gmail.com>
2023-07-20 10:27:00 -0700

    spacesg

--- pgml-extension/build.rs
+++ pgml-extension/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-	#[cfg(target_os = "macos")]
+    #[cfg(target_os = "macos")]
     {
         println!("cargo:rustc-link-search=/opt/homebrew/opt/openblas/lib");
         println!("cargo:rustc-link-search=/opt/homebrew/opt/libomp/lib/");
