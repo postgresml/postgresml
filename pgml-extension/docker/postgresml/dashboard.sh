2eb1083bbdd74c49ce17c48f63acbdb8f20006bd
Lev Kokotov <lev.kokotov@gmail.com>
2023-07-23 02:47:18 -0700

    content

--- pgml-extension/docker/postgresml/dashboard.sh
+++ pgml-extension/docker/postgresml/dashboard.sh
@@ -3,7 +3,7 @@ set -e
 
 export DATABASE_URL=postgres:///postgres
 export DASHBOARD_STATIC_DIRECTORY=/usr/share/pgml-dashboard/dashboard-static
-export DASHBOARD_CONTENT_DIRECTORY=/usr/share/pgml-dashboard/content
+export DASHBOARD_CONTENT_DIRECTORY=/usr/share/pgml-dashboard/dashboard-content
 export ROCKET_SECRET_KEY=$(openssl rand -hex 32)
 export ROCKET_ADDRESS=0.0.0.0
 export RUST_LOG=info
