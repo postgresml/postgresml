27b1f33c52e5d51634330430b2604e388b128ae3
Lev Kokotov <levkk@users.noreply.github.com>
2023-07-23 16:10:54 -0700

    more docs (#862)

--- pgml-extension/docker/postgresml/entrypoint.sh
+++ pgml-extension/docker/postgresml/entrypoint.sh
@@ -2,8 +2,24 @@
 #
 # Start local dev
 #
-echo "Starting PostgresML, one moment"
+echo "Starting PostgresML"
 service postgresql start
-echo "Ready"
+
+# Setup users
+useradd postgresml -m 2> /dev/null 1>&2
+sudo -u postgresml touch /home/postgresml/.psql_history
+sudo -u postgres createuser root --superuser --login 2> /dev/null 1>&2
+sudo -u postgres psql -c "CREATE ROLE postgresml PASSWORD 'postgresml' SUPERUSER LOGIN" 2> /dev/null 1>&2
+sudo -u postgres createdb postgresml --owner postgresml 2> /dev/null 1>&2
+sudo -u postgres psql -c 'ALTER ROLE postgresml SET search_path TO public,pgml' 2> /dev/null 1>&2
+
+echo "Starting dashboard"
+PGPASSWORD=postgresml psql -c 'CREATE EXTENSION IF NOT EXISTS pgml' \
+	-d postgresml \
+	-U postgresml \
+	-h 127.0.0.1 \
+	-p 5432 2> /dev/null 1>&2
+
+bash /app/dashboard.sh &
 
 exec "$@"
