787f165b7b69ead53b1ed68310909889a61f984e
Lev Kokotov <lev.kokotov@gmail.com>
2023-08-31 10:55:52 -0700

    sort paths

--- pgml-dashboard/src/components/mod.rs
+++ pgml-dashboard/src/components/mod.rs
@@ -5,57 +5,57 @@ mod component;
 pub(crate) use component::{component, Component};
 
 
-// src/components/navbar
-pub mod navbar;
-pub use navbar::Navbar;
-
-// src/components/static_nav_link
-pub mod static_nav_link;
-pub use static_nav_link::StaticNavLink;
-
-// src/components/left_nav_web_app
-pub mod left_nav_web_app;
-pub use left_nav_web_app::LeftNavWebApp;
-
-// src/components/left_nav_menu
-pub mod left_nav_menu;
-pub use left_nav_menu::LeftNavMenu;
-
-// src/components/static_nav
-pub mod static_nav;
-pub use static_nav::StaticNav;
-
-// src/components/nav_link
-pub mod nav_link;
-pub use nav_link::NavLink;
+// src/components/breadcrumbs
+pub mod breadcrumbs;
+pub use breadcrumbs::Breadcrumbs;
 
 // src/components/confirm_modal
 pub mod confirm_modal;
 pub use confirm_modal::ConfirmModal;
 
-// src/components/navbar_web_app
-pub mod navbar_web_app;
-pub use navbar_web_app::NavbarWebApp;
+// src/components/github_icon
+pub mod github_icon;
+pub use github_icon::GithubIcon;
 
-// src/components/nav
-pub mod nav;
-pub use nav::Nav;
+// src/components/left_nav_menu
+pub mod left_nav_menu;
+pub use left_nav_menu::LeftNavMenu;
+
+// src/components/left_nav_web_app
+pub mod left_nav_web_app;
+pub use left_nav_web_app::LeftNavWebApp;
 
 // src/components/modal
 pub mod modal;
 pub use modal::Modal;
 
+// src/components/nav
+pub mod nav;
+pub use nav::Nav;
+
+// src/components/nav_link
+pub mod nav_link;
+pub use nav_link::NavLink;
+
+// src/components/navbar
+pub mod navbar;
+pub use navbar::Navbar;
+
+// src/components/navbar_web_app
+pub mod navbar_web_app;
+pub use navbar_web_app::NavbarWebApp;
+
 // src/components/postgres_logo
 pub mod postgres_logo;
 pub use postgres_logo::PostgresLogo;
 
-// src/components/github_icon
-pub mod github_icon;
-pub use github_icon::GithubIcon;
+// src/components/static_nav
+pub mod static_nav;
+pub use static_nav::StaticNav;
 
-// src/components/breadcrumbs
-pub mod breadcrumbs;
-pub use breadcrumbs::Breadcrumbs;
+// src/components/static_nav_link
+pub mod static_nav_link;
+pub use static_nav_link::StaticNavLink;
 
 // src/components/test_component
 pub mod test_component;
