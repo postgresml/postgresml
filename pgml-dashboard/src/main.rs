416137666067880f63fe918d5fc9efb46486f0c9
Montana Low <montana.low@gmail.com>
2023-10-11 19:53:01 -0700

    remove README hardcoded links

--- pgml-dashboard/src/main.rs
+++ pgml-dashboard/src/main.rs
@@ -285,7 +285,7 @@ mod test {
     #[rocket::async_test]
     async fn test_docs() {
         let client = Client::tracked(rocket().await).await.unwrap();
-        let response = client.get("/docs/guides/README").dispatch().await;
+        let response = client.get("/docs/guides/").dispatch().await;
         assert_eq!(response.status().code, 200);
     }
 
