75b0b7eea2f0b2fc20bc08e94ea36dc9a23bc38f
Dan <39170265+chillenberger@users.noreply.github.com>
2023-10-27 10:33:27 -0600

    add list item to take color, small refactors (#1126)

--- pgml-dashboard/src/components/lists/item/mod.rs
+++ pgml-dashboard/src/components/lists/item/mod.rs
@@ -1,21 +1,51 @@
 use pgml_components::component;
 use sailfish::TemplateOnce;
+use std::fmt;
+
+#[derive(PartialEq, Eq, Default, Clone)]
+pub enum Color {
+    #[default]
+    Green,
+    Blue,
+    Orange,
+    Pink,
+    Purple,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Color::Green => write!(f, "green"),
+            Color::Blue => write!(f, "blue"),
+            Color::Orange => write!(f, "orange"),
+            Color::Pink => write!(f, "pink"),
+            Color::Purple => write!(f, "purple"),
+        }
+    }
+}
 
 #[derive(TemplateOnce, Default)]
 #[template(path = "lists/item/template.html")]
 pub struct Item {
     value: String,
+    color: Color,
 }
 
 impl Item {
     pub fn new() -> Item {
         Item {
             value: String::from("Your list item"),
+            color: Color::Green,
         }
     }
 
     pub fn value(mut self, value: &str) -> Item {
         self.value = value.into();
+        self
+    }
+
+    pub fn color(mut self, color: Color) -> Item {
+        self.color = color;
         self
     }
 }
