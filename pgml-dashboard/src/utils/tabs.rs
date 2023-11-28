22f6e41aad0e2a5cb3259ceb7b37c099e3b54e86
Montana Low <montanalow@users.noreply.github.com>
2023-11-27 21:47:06 -0600

    Montana/norly (#1194)

--- pgml-dashboard/src/utils/tabs.rs
+++ pgml-dashboard/src/utils/tabs.rs
@@ -19,16 +19,17 @@ impl<'a> Tabs<'a> {
     ) -> anyhow::Result<Self> {
         let default = match default {
             Some(default) => default,
-            _ => tabs
-                .get(0)
-                .ok_or(anyhow!("There must be at least one tab."))?
-                .name,
+            _ => {
+                tabs.get(0)
+                    .ok_or(anyhow!("There must be at least one tab."))?
+                    .name
+            }
         };
 
         let active = active
             .and_then(|name| {
                 let found = tabs.iter().find(|tab| tab.name == name);
-                
+
                 found.map(|tab| tab.name)
             })
             .unwrap_or(default);
