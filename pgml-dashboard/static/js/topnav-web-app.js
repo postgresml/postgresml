c03bde62500181b47efc3c2b184692da092c6387
Dan <39170265+chillenberger@users.noreply.github.com>
2023-12-01 16:23:47 -0700

    fix breadcrumb, nav, bug

--- pgml-dashboard/static/js/topnav-web-app.js
+++ pgml-dashboard/static/js/topnav-web-app.js
@@ -3,20 +3,25 @@ import {
 } from '@hotwired/stimulus'
 
 export default class extends Controller {
-
     connect() {
-        let navbarMenues = document.querySelectorAll('.navbar-collapse');
+        document.addEventListener('show.bs.collapse', this.closeOtherMenus)
+        document.addEventListener('hidden.bs.collapse', this.closeSubmenus, false)
+    }
 
-        document.addEventListener('show.bs.collapse', e => {
-            this.closeOtherMenues(navbarMenues, e.target)
-        })
-
-        document.addEventListener('hidden.bs.collapse', e => {
-            this.closeSubmenus(e.target.querySelectorAll('.drawer-submenu'))
+    closeSubmenus(e) {
+        let submenus = e.target.querySelectorAll('.drawer-submenu')
+        submenus.forEach(submenu => {
+            const bsInstance = bootstrap.Collapse.getInstance(submenu)
+            if ( bsInstance ) {
+                bsInstance.hide()
+            }
         })
     }
 
-    closeOtherMenues(menus, current) {
+    closeOtherMenus(e) {
+        let menus = document.querySelectorAll('.navbar-collapse')
+        let current = e.target
+
         menus.forEach( menu => {
             const bsInstance = bootstrap.Collapse.getInstance(menu)
             if ( bsInstance && menu != current && menu != current.parentElement ) {
@@ -25,12 +30,8 @@ export default class extends Controller {
         })
     }
 
-    closeSubmenus(submenues) {
-        submenues.forEach(submenu => {
-            const bsInstance = bootstrap.Collapse.getInstance(submenu)
-            if ( bsInstance ) {
-                bsInstance.hide()
-            }
-        })
+    disconnect() {
+        document.removeEventListener('show.bs.collapse', this.closeOtherMenus)
+        document.removeEventListener('hidden.bs.collapse', this.closeSubmenus)
     }
 }
