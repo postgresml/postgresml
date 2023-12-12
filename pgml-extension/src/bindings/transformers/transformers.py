189332d8e4cd17033646e382b3278c9b57c9be67
Montana Low <montana.low@gmail.com>
2023-12-11 21:06:28 -0800

    update Cargo.lock

--- pgml-extension/src/bindings/transformers/transformers.py
+++ pgml-extension/src/bindings/transformers/transformers.py
@@ -391,26 +391,41 @@ def transform(task, args, inputs, stream=False):
     return orjson.dumps(pipe(inputs, **args), default=orjson_default).decode()
 
 
-def embed(transformer, inputs, kwargs):
-    kwargs = orjson.loads(kwargs)
-    ensure_device(kwargs)
+def create_embedding(transformer):
     instructor = transformer.startswith("hkunlp/instructor")
+    klass = INSTRUCTOR if instructor else SentenceTransformer
+    return klass(transformer)
 
-    # Cache the model
-    if transformer not in __cache_sentence_transformer_by_name:
-        klass = INSTRUCTOR if instructor else SentenceTransformer
-        __cache_sentence_transformer_by_name[transformer] = klass(transformer)
-    model = __cache_sentence_transformer_by_name[transformer]
 
-    # Handle instruction encoding
+def embed_using(model, transformer, inputs, kwargs):
+    if isinstance(kwargs, str):
+        kwargs = orjson.loads(kwargs)
+
+    instructor = transformer.startswith("hkunlp/instructor")
     if instructor:
         texts_with_instructions = []
         instruction = kwargs.pop("instruction")
         for text in inputs:
             texts_with_instructions.append([instruction, text])
+
         inputs = texts_with_instructions
 
     return model.encode(inputs, **kwargs)
+
+
+def embed(transformer, inputs, kwargs):
+    kwargs = orjson.loads(kwargs)
+
+    ensure_device(kwargs)
+
+    if transformer not in __cache_sentence_transformer_by_name:
+        __cache_sentence_transformer_by_name[transformer] = create_embedding(
+            transformer
+        )
+    model = __cache_sentence_transformer_by_name[transformer]
+
+    return embed_using(model, transformer, inputs, kwargs)
+
 
 
 def clear_gpu_cache(memory_usage: None):
