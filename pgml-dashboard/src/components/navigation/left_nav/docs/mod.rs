8eee967f19003011c4fb61c3592c020bb65dbdd8
Dan <39170265+chillenberger@users.noreply.github.com>
2024-02-16 15:48:28 -0700

    TOC, cleanup scrollbars

--- pgml-dashboard/src/components/navigation/left_nav/docs/mod.rs
+++ pgml-dashboard/src/components/navigation/left_nav/docs/mod.rs
@@ -6,11 +6,20 @@ use sailfish::TemplateOnce;
 #[template(path = "navigation/left_nav/docs/template.html")]
 pub struct Docs {
     index: Vec<IndexLink>,
+    mobile: bool,
 }
 
 impl Docs {
     pub fn new(index: &Vec<IndexLink>) -> Docs {
-        Docs { index: index.clone() }
+        Docs {
+            index: index.clone(),
+            mobile: false,
+        }
+    }
+
+    pub fn for_mobile(mut self) -> Docs {
+        self.mobile = true;
+        self
     }
 }
 
