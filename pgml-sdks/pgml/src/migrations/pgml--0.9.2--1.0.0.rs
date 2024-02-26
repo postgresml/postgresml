4fb0149160929caf8174f0a2301fa904baa0058e
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2024-02-26 11:46:34 -0800

    Updated text

--- pgml-sdks/pgml/src/migrations/pgml--0.9.2--1.0.0.rs
+++ pgml-sdks/pgml/src/migrations/pgml--0.9.2--1.0.0.rs
@@ -4,6 +4,6 @@ use tracing::instrument;
 #[instrument(skip(_pool))]
 pub async fn migrate(_pool: PgPool, _: Vec<i64>) -> anyhow::Result<String> {
     anyhow::bail!(
-        "There is no automatic migration to SDK version 1.0. Please just upgrade the SDK and create a new collection",
+        "There is no automatic migration to SDK version 1.0. Please upgrade the SDK and create a new collection, or contact your PostgresML support to create a migration plan.",
     )
 }
