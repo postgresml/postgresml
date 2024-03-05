4dc139e8fbfae4740a0344c15f4f729c93ee45a2
Dan <39170265+chillenberger@users.noreply.github.com>
2024-03-05 16:24:41 -0700

    move loading to dashboard, use loading for search

---
+++ pgml-dashboard/src/components/loading/message/mod.rs
@@ -0,0 +1,23 @@
+use sailfish::TemplateOnce;
+use pgml_components::component;
+
+#[derive(TemplateOnce, Default)]
+#[template(path = "loading/message/template.html")]
+pub struct Message {
+    message: String,
+}
+
+impl Message {
+    pub fn new() -> Message {
+        Message {
+            message: String::from("Loading..."),
+        }
+    }
+
+    pub fn message(mut self, message: &str) -> Message {
+        self.message = String::from(message);
+        self
+    }
+}
+
+component!(Message);
