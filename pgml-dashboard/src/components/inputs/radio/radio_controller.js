57089fd1b9674f0d75b1aac04dacb1e3fd4d1575
Lev Kokotov <lev.kokotov@gmail.com>
2024-04-01 12:03:58 -0700

    radio + debug on Component

--- pgml-dashboard/src/components/inputs/radio/radio_controller.js
+++ pgml-dashboard/src/components/inputs/radio/radio_controller.js
@@ -2,15 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["button"];
-  static outlets = [];
 
   onClick(e) {
     this.buttonTargets.forEach((target) => {
       target.classList.remove("active");
       target.ariaPressed = false;
+      target.querySelector("input").checked = false;
     });
 
     e.currentTarget.classList.add("active");
     e.currentTarget.ariaPressed = true;
+    e.currentTarget.querySelector("input").checked = true;
   }
 }
--- pgml-dashboard/src/components/inputs/range_group_v_2/range_group_v_2_controller.js
+++ pgml-dashboard/src/components/inputs/range_group_v_2/range_group_v_2_controller.js
@@ -1,13 +1,13 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["input", "range"]
+  static targets = ["input", "range"];
 
   onInputChange(e) {
-    this.rangeTarget.value = e.currentTarget.value
+    this.rangeTarget.value = e.currentTarget.value;
   }
 
   onRangeChange(e) {
-    this.inputTarget.value = e.currentTarget.value
+    this.inputTarget.value = e.currentTarget.value;
   }
 }
