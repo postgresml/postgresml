d77a085f6f05095fef33e08b3b176d4cf5325b40
Lev <lev.kokotov@gmail.com>
2024-04-02 13:12:25 -0700

    Onboarding fixes

--- pgml-dashboard/src/components/cards/rgb/mod.rs
+++ pgml-dashboard/src/components/cards/rgb/mod.rs
@@ -5,8 +5,10 @@ use sailfish::TemplateOnce;
 #[template(path = "cards/rgb/template.html")]
 pub struct Rgb {
     value: Component,
-    active: bool,
     link: Option<String>,
+    controller_classes: Vec<String>,
+    card_classes: Vec<String>,
+    body_classes: Vec<String>,
 }
 
 impl Default for Rgb {
@@ -19,18 +21,27 @@ impl Rgb {
     pub fn new(value: Component) -> Rgb {
         Rgb {
             value,
-            active: false,
             link: None,
+            controller_classes: vec![],
+            card_classes: vec![],
+            body_classes: vec![],
         }
     }
 
     pub fn active(mut self) -> Self {
-        self.active = true;
+        self.card_classes.push("active".into());
+        self.card_classes.push("main-gradient-border-card-1".into());
         self
     }
 
     pub fn link(mut self, link: &str) -> Self {
         self.link = Some(link.to_string());
+        self
+    }
+
+    pub fn h_100(mut self) -> Self {
+        self.controller_classes.push("h-100".into());
+        self.card_classes.push("h-100".into());
         self
     }
 }
