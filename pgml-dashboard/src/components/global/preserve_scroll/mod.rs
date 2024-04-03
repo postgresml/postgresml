39f901c346202040cf85550ca594140a446ae0db
Lev Kokotov <lev.kokotov@gmail.com>
2024-04-03 15:34:23 -0700

    warning

--- pgml-dashboard/src/components/global/preserve_scroll/mod.rs
+++ pgml-dashboard/src/components/global/preserve_scroll/mod.rs
@@ -3,15 +3,11 @@ use sailfish::TemplateOnce;
 
 #[derive(TemplateOnce, Default)]
 #[template(path = "global/preserve_scroll/template.html")]
-pub struct PreserveScroll {
-    value: String,
-}
+pub struct PreserveScroll {}
 
 impl PreserveScroll {
     pub fn new() -> PreserveScroll {
-        PreserveScroll {
-            value: String::from("src/components/global/preserve_scroll"),
-        }
+        PreserveScroll {}
     }
 }
 
