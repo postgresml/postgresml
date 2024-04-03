c2e4efcbe1461dfb499fa562297113ef4037ebd3
Lev Kokotov <lev.kokotov@gmail.com>
2024-04-03 14:08:32 -0700

    zindex

--- pgml-dashboard/src/components/cards/primary/mod.rs
+++ pgml-dashboard/src/components/cards/primary/mod.rs
@@ -5,11 +5,20 @@ use sailfish::TemplateOnce;
 #[template(path = "cards/primary/template.html")]
 pub struct Primary {
     component: Component,
+    style: String,
 }
 
 impl Primary {
     pub fn new(component: Component) -> Primary {
-        Primary { component }
+        Primary {
+            component,
+            style: "".into(),
+        }
+    }
+
+    pub fn z_index(mut self, index: i64) -> Self {
+        self.style = format!("position: relative; z-index: {};", index);
+        self
     }
 }
 
