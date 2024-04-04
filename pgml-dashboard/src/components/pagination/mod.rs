df922734f7146fd31efed5851ef1462ca231b965
Dan <39170265+chillenberger@users.noreply.github.com>
2024-04-04 14:47:33 -0600

    remove dead code

--- pgml-dashboard/src/components/pagination/mod.rs
+++ pgml-dashboard/src/components/pagination/mod.rs
@@ -5,7 +5,6 @@ use sailfish::TemplateOnce;
 #[template(path = "pagination/template.html")]
 pub struct Pagination {
     count: usize,
-    default_index: usize,
     timed: bool,
     identifier: u16,
 }
@@ -14,15 +13,9 @@ impl Pagination {
     pub fn new(count: usize, identifier: u16) -> Pagination {
         Pagination {
             count,
-            default_index: 0,
             timed: false,
             identifier: identifier,
         }
-    }
-
-    pub fn default_index(mut self, default_index: usize) -> Self {
-        self.default_index = default_index;
-        self
     }
 
     pub fn timed(mut self) -> Self {
