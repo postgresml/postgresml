842360796997efa7e7d3e88f6f6a7eba325ac706
Lev <lev.kokotov@gmail.com>
2024-04-11 12:00:13 -0700

    Custom greeting area in split

--- pgml-dashboard/src/components/sections/split/mod.rs
+++ pgml-dashboard/src/components/sections/split/mod.rs
@@ -1,3 +1,5 @@
+//! Left/right split used in onboarding, signup, careers, etc.
+
 use pgml_components::component;
 use pgml_components::Component;
 use sailfish::TemplateOnce;
@@ -5,29 +7,45 @@ use sailfish::TemplateOnce;
 #[derive(TemplateOnce, Default)]
 #[template(path = "sections/split/template.html")]
 pub struct Split {
-    eyebrow: Component,
-    title: Component,
+    greeting_area: Component,
     display_area: Component,
     with_navbar: bool,
+}
+
+// Greeting with its own styling.
+#[derive(TemplateOnce, Default, Clone)]
+#[template(path = "sections/split/greeting.html")]
+pub struct Greeting {
+    eyebrow: Component,
+    title: Component,
+}
+
+component!(Greeting);
+
+impl Greeting {
+    pub fn new(eyebrow: Component, title: Component) -> Greeting {
+        Greeting { eyebrow, title }
+    }
 }
 
 impl Split {
     pub fn new() -> Split {
         Split {
-            eyebrow: Component::from(String::from("")),
-            title: Component::from(String::from("")),
-            display_area: Component::from(String::from("")),
+            greeting_area: Component::default(),
+            display_area: Component::default(),
             with_navbar: false,
         }
     }
 
-    pub fn eyebrow(mut self, eyebrow: Component) -> Split {
-        self.eyebrow = eyebrow;
+    // Set the greeting.
+    pub fn greeting(mut self, eyebrow: Component, title: Component) -> Split {
+        self.greeting_area = Greeting::new(eyebrow, title).into();
         self
     }
 
-    pub fn title(mut self, title: Component) -> Split {
-        self.title = title;
+    // Set whatever you want on the left side of the display.
+    pub fn greeting_area(mut self, greeting_area: Component) -> Split {
+        self.greeting_area = greeting_area;
         self
     }
 
