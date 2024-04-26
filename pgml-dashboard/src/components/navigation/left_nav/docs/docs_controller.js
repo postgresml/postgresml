5cd62100d4230a9f6729d9a6ce5ed82631b4325f
Dan <39170265+chillenberger@users.noreply.github.com>
2024-04-26 13:53:59 -0600

    move expoand to fe

--- pgml-dashboard/src/components/navigation/left_nav/docs/docs_controller.js
+++ pgml-dashboard/src/components/navigation/left_nav/docs/docs_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["level1Container", "level1Link", "highLevels"];
+  static targets = ["level1Container", "level1Link", "highLevels", "leftNav"];
 
   // After page update we reset scroll position of nav back to where it
   // was and ensure left nave and window location match.
-  // Stimulus connect runs on every page load regardless of the element
-  // being permanent or not.
   connect() {
     let nav = document.getElementsByClassName("doc-leftnav");
     if (nav.length > 0) {
@@ -14,10 +12,14 @@ export default class extends Controller {
       nav[0].scrollTop = position;
     }
 
-    this.setNavToLocation();
+    this.callback = () => {
+      this.setNavToLocation();
+    };
+
+    document.addEventListener("turbo:load", this.callback);
   }
 
-  // The active tags should always be set the current page location
+  // The active tags should always be set to the current page location
   setNavToLocation() {
     const tag = "a[href='" + window.location.pathname + "']";
 
@@ -34,21 +36,62 @@ export default class extends Controller {
     }
   }
 
+  expandSubmenuIfExists(containerEl) {
+    const controllerEl = containerEl.querySelector(
+      "[data-action='click->navigation-left-nav-docs#toggle']",
+    );
+    controllerEl ? this.expand(controllerEl) : null;
+  }
+
+  // Finds all parent submenus this element is in and expands them. Takes
+  // the element containing the current level
+  expandAllParents(element) {
+    let level = element.getAttribute("data-level");
+
+    this.expandSubmenuIfExists(element);
+    if (level > 1) {
+      let next = "div[data-level='" + (parseInt(level) - 1) + "']";
+      this.expandAllParents(element.closest(next));
+    }
+  }
+
   // turbo-frame-permanent breaks bootstrap data attribute collapse for aria
   // so we manually control collapse
-  expand(e) {
-    let aria = e.currentTarget.getAttribute("aria-expanded");
-    let id = e.currentTarget.getAttribute("aria-controls");
+  toggle(event) {
+    let aria = event.currentTarget.getAttribute("aria-expanded");
 
-    let bsCollapse = bootstrap.Collapse.getOrCreateInstance(
-      document.getElementById(id),
-    );
     if (aria === "true") {
-      bsCollapse.hide();
-      e.currentTarget.setAttribute("aria-expanded", "false");
+      this.collapse(event.currentTarget);
     } else {
+      this.expand(event.currentTarget);
+    }
+  }
+
+  // Expands the submenu, takes submenu control element.
+  expand(element) {
+    let id = element.getAttribute("aria-controls");
+    let aria = element.getAttribute("aria-expanded");
+
+    if (aria === "false") {
+      let bsCollapse = bootstrap.Collapse.getOrCreateInstance(
+        document.getElementById(id),
+      );
       bsCollapse.show();
-      e.currentTarget.setAttribute("aria-expanded", "true");
+      element.setAttribute("aria-expanded", "true");
+    }
+  }
+
+  // Collapses the submenu, takes submenu control element.
+  collapse(element) {
+    let id = element.getAttribute("aria-controls");
+    let aria = element.getAttribute("aria-expanded");
+
+    if (aria === "true") {
+      let bsCollapse = bootstrap.Collapse.getOrCreateInstance(
+        document.getElementById(id),
+      );
+      bsCollapse.hide();
+      element.setAttribute("aria-expanded", "false");
     }
   }
 
@@ -56,12 +99,21 @@ export default class extends Controller {
   setHighLevelLeftNav(element) {
     this.removeAllActive();
 
-    let container = element.closest('div[data-level="1"]');
-    let menu = container.querySelector(".menu-item");
-    let link = menu.querySelector(".doc-left-nav-level1-link-container");
+    const parentContainer = element.closest('div[data-level="1"]');
+    const parentMenu = parentContainer.querySelector(".menu-item");
+    const parentLink = parentMenu.querySelector(
+      ".doc-left-nav-level1-link-container",
+    );
 
-    link.classList.add("active");
+    parentLink.classList.add("active");
     element.classList.add("purple");
+
+    const container = element.parentElement;
+    this.expandSubmenuIfExists(container);
+
+    const levelEl = container.closest("div[data-level]");
+    this.expandAllParents(levelEl);
+
     this.preventScrollOnNav();
   }
 
@@ -69,10 +121,12 @@ export default class extends Controller {
   setLevel1LeftNav(element) {
     this.removeAllActive();
 
-    let container = element.closest("div");
+    const container = element.closest("div");
     container.classList.add("active");
 
     element.classList.add("active");
+
+    this.expandSubmenuIfExists(container);
 
     this.preventScrollOnNav();
   }
@@ -92,10 +146,9 @@ export default class extends Controller {
   // turbo-frame permanent scrolls nav to top on navigation so we capture the scroll position prior
   // to updating the page so after we can set the scroll position back to where it was
   preventScrollOnNav() {
-    let nav = document.getElementsByClassName("doc-leftnav");
-    if (nav.length > 0) {
-      let position = nav[0].scrollTop;
-      nav[0].setAttribute("data-scroll", position);
+    if (this.hasLeftNavTarget) {
+      let position = this.leftNavTarget.scrollTop;
+      this.leftNavTarget.setAttribute("data-scroll", position);
     }
   }
 
@@ -112,5 +165,9 @@ export default class extends Controller {
     for (let i = 0; i < this.level1LinkTargets.length; i++) {
       this.level1LinkTargets[i].classList.remove("active");
     }
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:load", this.callback);
   }
 }
