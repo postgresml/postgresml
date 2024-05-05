2e68f0c16c9828c8c9a60d33a87be0482daa8feb
Montana Low <montana.low@gmail.com>
2024-05-05 16:15:07 -0700

    sql syntax

--- pgml-extension/sql/pgml--2.8.3--2.8.4.sql
+++ pgml-extension/sql/pgml--2.8.3--2.8.4.sql
@@ -1,12 +1,12 @@
+ALTER TYPE pgml.task RENAME VALUE 'cluster' TO 'clustering';
 ALTER TYPE pgml.task ADD VALUE IF NOT EXISTS 'decomposition';
 
 ALTER TYPE pgml.algorithm ADD VALUE IF NOT EXISTS 'pca';
-ALTER TYPE pgml.task RENAME VALUE 'cluster' TO 'clustering';
 
 -- pgml::api::decompose
 CREATE FUNCTION pgml."decompose"(
     "project_name" TEXT, /* alloc::string::String */
-    "vector" FLOAT4[], /* Vec<f32> */
+    "vector" FLOAT4[] /* Vec<f32> */
 ) RETURNS FLOAT4[] /* Vec<f32> */
     IMMUTABLE STRICT PARALLEL SAFE
 LANGUAGE c /* Rust */
