2724e59d570b9aaa6dfe95a6bb49b4d785e7b144
Silas Marvin <19626586+SilasMarvin@users.noreply.github.com>
2024-05-28 13:34:28 -0700

    Remove duplicate key

--- pgml-sdks/pgml/src/open_source_ai.rs
+++ pgml-sdks/pgml/src/open_source_ai.rs
@@ -43,17 +43,6 @@ fn try_model_nice_name_to_model_name_and_parameters(
             .into(),
         )),
 
-        "meta-llama/Meta-Llama-3-8B-Instruct" => Some((
-            "meta-llama/Meta-Llama-3-8B-Instruct",
-            serde_json::json!({
-              "task": "conversational",
-              "model": "meta-llama/Meta-Llama-3-8B-Instruct",
-              "device_map": "auto",
-              "torch_dtype": "bfloat16"
-            })
-            .into(),
-        )),
-
         "TheBloke/Llama-2-7B-Chat-GPTQ" => Some((
             "TheBloke/Llama-2-7B-Chat-GPTQ",
             serde_json::json!({
