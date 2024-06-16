e1f64d66863ca8c65b4b82f1a2df04095902f9d3
Dan <39170265+chillenberger@users.noreply.github.com>
2024-06-16 11:59:36 -0600

    add tests

--- pgml-dashboard/src/utils/cookies.rs
+++ pgml-dashboard/src/utils/cookies.rs
@@ -2,7 +2,7 @@ use chrono;
 use rocket::http::{Cookie, CookieJar};
 use rocket::serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, Default, PartialEq)]
 pub struct NotificationCookie {
     pub id: String,
     pub time_viewed: Option<chrono::DateTime<chrono::Utc>>,
@@ -12,51 +12,113 @@ pub struct NotificationCookie {
 pub struct Notifications {}
 
 impl Notifications {
-    pub fn update_viewed(new: &Vec<NotificationCookie>, cookies: &CookieJar<'_>) {
-        let serialized = new
-            .iter()
-            .map(|x| serde_json::to_string(x).unwrap())
-            .collect::<Vec<String>>();
+    pub fn update_viewed(all_desired_notifications: &Vec<NotificationCookie>, cookies: &CookieJar<'_>) {
+        let session = Notifications::safe_serialize_session(all_desired_notifications);
 
-        let mut cookie = Cookie::new("session", format!(r#"{{"notifications": [{}]}}"#, serialized.join(",")));
+        let mut cookie = Cookie::new("session", session);
         cookie.set_max_age(::time::Duration::weeks(4));
         cookies.add_private(cookie);
     }
 
     pub fn get_viewed(cookies: &CookieJar<'_>) -> Vec<NotificationCookie> {
-        let viewed: Vec<NotificationCookie> = match cookies.get_private("session") {
-            Some(session) => {
-                match serde_json::from_str::<serde_json::Value>(session.value())
-                    .unwrap_or_else(|_| serde_json::from_str::<serde_json::Value>(r#"{"notifications": []}"#).unwrap())
-                    ["notifications"]
-                    .as_array()
-                {
-                    Some(items) => items
-                        .into_iter()
-                        .map(|x| {
-                            serde_json::from_str::<NotificationCookie>(&x.to_string()).unwrap_or_else(|_| {
-                                serde_json::from_str::<String>(&x.to_string())
-                                    .and_then(|z| {
-                                        Ok(NotificationCookie {
-                                            id: z,
-                                            time_viewed: None,
-                                            time_modal_viewed: None,
-                                        })
-                                    })
-                                    .unwrap_or_else(|_| NotificationCookie {
-                                        id: "".to_string(),
-                                        time_viewed: None,
-                                        time_modal_viewed: None,
-                                    })
-                            })
-                        })
-                        .collect::<Vec<NotificationCookie>>(),
-                    _ => vec![],
-                }
-            }
+        match cookies.get_private("session") {
+            Some(session) => Notifications::safe_deserialize_session(session.value()),
             None => vec![],
-        };
+        }
+    }
 
-        viewed
+    pub fn safe_deserialize_session(session: &str) -> Vec<NotificationCookie> {
+        match serde_json::from_str::<serde_json::Value>(session).unwrap_or_else(|_| {
+            serde_json::from_str::<serde_json::Value>(&Notifications::safe_serialize_session(&vec![])).unwrap()
+        })["notifications"]
+            .as_array()
+        {
+            Some(items) => items
+                .into_iter()
+                .map(|notification| {
+                    serde_json::from_str::<NotificationCookie>(&notification.to_string()).unwrap_or_else(|_| {
+                        serde_json::from_str::<String>(&notification.to_string())
+                            .and_then(|id| {
+                                Ok(NotificationCookie {
+                                    id,
+                                    time_viewed: None,
+                                    time_modal_viewed: None,
+                                })
+                            })
+                            .unwrap_or_else(|_| NotificationCookie::default())
+                    })
+                })
+                .collect::<Vec<NotificationCookie>>(),
+            _ => vec![],
+        }
+    }
+
+    pub fn safe_serialize_session(cookies: &Vec<NotificationCookie>) -> String {
+        let serialized = cookies
+            .iter()
+            .map(|x| serde_json::to_string(x))
+            .filter(|x| x.is_ok())
+            .map(|x| x.unwrap())
+            .collect::<Vec<String>>();
+
+        format!(r#"{{"notifications": [{}]}}"#, serialized.join(","))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // Test that we can safely deserialize expected session data.
+    #[test]
+    fn test_safe_deserialize_session() {
+        let session = r#"{"notifications": [{"id": "1", "time_viewed": null, "time_modal_viewed": null}, {"id": "1234567891234", "time_viewed": "2021-08-01T00:00:00Z"}]}"#;
+        let expected = vec![
+            NotificationCookie {
+                id: "1".to_string(),
+                time_viewed: None,
+                time_modal_viewed: None,
+            },
+            NotificationCookie {
+                id: "1234567891234".to_string(),
+                time_viewed: Some(
+                    chrono::DateTime::parse_from_rfc3339("2021-08-01T00:00:00Z")
+                        .unwrap()
+                        .into(),
+                ),
+                time_modal_viewed: None,
+            },
+        ];
+        assert_eq!(Notifications::safe_deserialize_session(session), expected);
+    }
+
+    // Test that new notification system is backwards compatible.
+    #[test]
+    fn test_safe_deserialize_session_old_form() {
+        let session = r#"{"notifications": ["123456789"]}"#;
+        let expected = vec![NotificationCookie {
+            id: "123456789".to_string(),
+            time_viewed: None,
+            time_modal_viewed: None,
+        }];
+        assert_eq!(Notifications::safe_deserialize_session(session), expected);
+    }
+
+    #[test]
+    fn test_safe_deserialize_session_empty() {
+        let session = r#"{}"#;
+        let expected: Vec<NotificationCookie> = vec![];
+        assert_eq!(Notifications::safe_deserialize_session(session), expected);
+    }
+
+    #[test]
+    fn test_safe_serialize_session() {
+        let cookies = vec![NotificationCookie {
+            id: "1".to_string(),
+            time_viewed: None,
+            time_modal_viewed: None,
+        }];
+        let expected = r#"{"notifications": [{"id":"1","time_viewed":null,"time_modal_viewed":null}]}"#;
+        assert_eq!(Notifications::safe_serialize_session(&cookies), expected);
     }
 }
