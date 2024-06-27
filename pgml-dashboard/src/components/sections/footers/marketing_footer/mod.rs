41ceca27c95f6aaee51af360a444643e72f44ef6
Dan <39170265+chillenberger@users.noreply.github.com>
2024-06-27 16:55:13 -0600

    add pgcat icon as font, update footer

--- pgml-dashboard/src/components/sections/footers/marketing_footer/mod.rs
+++ pgml-dashboard/src/components/sections/footers/marketing_footer/mod.rs
@@ -5,6 +5,7 @@ use sailfish::TemplateOnce;
 #[derive(TemplateOnce, Default)]
 #[template(path = "sections/footers/marketing_footer/template.html")]
 pub struct MarketingFooter {
+    product: Vec<StaticNavLink>,
     solutions: Vec<StaticNavLink>,
     resources: Vec<StaticNavLink>,
     company: Vec<StaticNavLink>,
@@ -13,12 +14,23 @@ pub struct MarketingFooter {
 impl MarketingFooter {
     pub fn new() -> MarketingFooter {
         MarketingFooter {
+            product: vec![
+                StaticNavLink::new("Korvus".into(), "https://github.com/postgresml/korvus".into()),
+                StaticNavLink::new("PGML".into(), "https://github.com/postgresml/postgresml".into()),
+                StaticNavLink::new("PpCat Learning".into(), "https://github.com/postgresml/pgcat".into()),
+                StaticNavLink::new("PostgresML".into(), "/docs/product/cloud-database/".into()),
+                StaticNavLink::new("VPC".into(), "/docs/product/vpn_key".into()).disabled(true),
+            ],
             solutions: vec![
-                StaticNavLink::new("Overview".into(), "/docs/".into()),
+                StaticNavLink::new("NLP".into(), "/docs/guides/natural-language-processing".into()),
+                StaticNavLink::new("Supervised Learning".into(), "/docs/guides/supervised-learning".into()),
+                StaticNavLink::new("Embedding".into(), "/docs/api/sql-extension/pgml.embed".into()),
+                StaticNavLink::new("Vector Database".into(), "/docs/product/vector-database".into()),
+                StaticNavLink::new(
+                    "Search".into(),
+                    "/docs/guides/improve-search-results-with-machine-learning".into(),
+                ),
                 StaticNavLink::new("Chatbot".into(), "/chatbot".into()),
-                StaticNavLink::new("Site Search".into(), "/search".into()).disabled(true),
-                StaticNavLink::new("Fraud Detection".into(), "/fraud".into()).disabled(true),
-                StaticNavLink::new("Forecasting".into(), "/forecasting".into()).disabled(true),
             ],
             resources: vec![
                 StaticNavLink::new("Documentation".into(), "/docs/".into()),
@@ -43,6 +55,11 @@ impl MarketingFooter {
 
     pub fn company(mut self, company: Vec<StaticNavLink>) -> MarketingFooter {
         self.company = company;
+        self
+    }
+
+    pub fn product(mut self, product: Vec<StaticNavLink>) -> MarketingFooter {
+        self.product = product;
         self
     }
 }
