af55b2681c874e1ac2db7c27c4fb11d9fa87f778
Montana Low <montana.low@gmail.com>
2025-01-17 11:55:21 -0800

    package for multiple versions of ubuntu

--- packages/postgresml-python/build.sh
+++ packages/postgresml-python/build.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-#
-#
-#
 set -e
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 deb_dir="/tmp/postgresml-python/deb-build"
-major=${1:-"14"}
 
+# Parse arguments with defaults
 export PACKAGE_VERSION=${1:-"2.10.0"}
-export PYTHON_VERSION=${2:-"3.10"}
+export UBUNTU_VERSION=${2:-"22.04"}
+export PYTHON_VERSION=${3:-"3.10"}
 
+# Handle architecture
 if [[ $(arch) == "x86_64" ]]; then
   export ARCH=amd64
 else
   export ARCH=arm64
+fi
+
+# Map Ubuntu versions to Python versions if needed
+# For example: Ubuntu 20.04 uses Python 3.8 by default
+declare -A ubuntu_python_versions=(
+  ["20.04"]="3.8"
+  ["22.04"]="3.10"
+  ["24.04"]="3.11"
+)
+
+if [[ -z "$3" ]]; then
+  PYTHON_VERSION=${ubuntu_python_versions[$UBUNTU_VERSION]:-"3.10"}
 fi
 
 rm -rf "$deb_dir"
@@ -23,10 +35,10 @@ cp -R ${SCRIPT_DIR}/* "$deb_dir"
 rm "$deb_dir/build.sh"
 rm "$deb_dir/release.sh"
 
-(cat ${SCRIPT_DIR}/DEBIAN/control | envsubst) > "$deb_dir/DEBIAN/control"
-(cat ${SCRIPT_DIR}/DEBIAN/postinst | envsubst '${PGVERSION}') > "$deb_dir/DEBIAN/postinst"
-(cat ${SCRIPT_DIR}/DEBIAN/prerm | envsubst '${PGVERSION}') > "$deb_dir/DEBIAN/prerm"
-(cat ${SCRIPT_DIR}/DEBIAN/postrm | envsubst '${PGVERSION}') > "$deb_dir/DEBIAN/postrm"
+(cat ${SCRIPT_DIR}/DEBIAN/control | envsubst '${PACKAGE_VERSION} ${UBUNTU_VERSION} ${ARCH} ${PYTHON_VERSION}') > "$deb_dir/DEBIAN/control"
+(cat ${SCRIPT_DIR}/DEBIAN/postinst | envsubst '${PGVERSION} ${PYTHON_VERSION}') > "$deb_dir/DEBIAN/postinst"
+(cat ${SCRIPT_DIR}/DEBIAN/prerm | envsubst '${PGVERSION} ${PYTHON_VERSION}') > "$deb_dir/DEBIAN/prerm"
+(cat ${SCRIPT_DIR}/DEBIAN/postrm | envsubst '${PGVERSION} ${PYTHON_VERSION}') > "$deb_dir/DEBIAN/postrm"
 
 if [[ "$ARCH" == "amd64" ]]; then
   cp ${SCRIPT_DIR}/../../pgml-extension/requirements.linux.txt "$deb_dir/etc/postgresml-python/requirements.txt"
@@ -34,7 +46,7 @@ else
   cp ${SCRIPT_DIR}/../../pgml-extension/requirements.macos.txt "$deb_dir/etc/postgresml-python/requirements.txt"
 fi
 
-virtualenv --python="python$PYTHON_VERSION" "$deb_dir/var/lib/postgresml-python/pgml-venv"
+virtualenv --python="python${PYTHON_VERSION}" "$deb_dir/var/lib/postgresml-python/pgml-venv"
 source "$deb_dir/var/lib/postgresml-python/pgml-venv/bin/activate"
 
 python -m pip install -r "${deb_dir}/etc/postgresml-python/requirements.txt"
@@ -48,6 +60,6 @@ dpkg-deb \
   --root-owner-group \
   -z1 \
   --build "$deb_dir" \
-  postgresml-python-${PACKAGE_VERSION}-ubuntu22.04-${ARCH}.deb
+  "postgresml-python-${PACKAGE_VERSION}-ubuntu${UBUNTU_VERSION}-${ARCH}.deb"
 
 rm -rf "$deb_dir"
